f150e3c80c9dff0aaf49363b5e4ff3110503e21a
Berheruben <rubenberhe56@yourdomain.com>
2024-06-16 14:09:45 +0200

    create homepage component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,15 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import RecipeDetails from "./pages/RecipeDetails";
+import HomePage from "./components/HomePage";
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<RecipeList />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipes" element={<RecipeList />} />
           <Route path="/add" element={<AddRecipe />} />
           <Route path="/recipes/:id" element={<RecipeDetails />} />
         </Routes>

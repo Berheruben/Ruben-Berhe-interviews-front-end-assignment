513a42abbf6b47e3f25b60023e85066f16e3bb15
Berheruben <rubenberhe56@yourdomain.com>
2024-06-18 11:05:21 +0200

    new api for fetching data

--- frontend/src/hooks/useRecipes.ts
+++ frontend/src/hooks/useRecipes.ts
@@ -1,17 +1,56 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const fetchRecipes = async (page: number, limit: number) => {
-    const { data } = await axios.get(`http://localhost:8080/recipes`, {
-        params: { _page: page, _limit: limit },
-    });
+const fetchCuisines = async () => {
+    const { data } = await axios.get('http://localhost:8080/cuisines');
     return data;
 };
 
-const useRecipes = (page: number, limit: number) => {
+const fetchDiets = async () => {
+    const { data } = await axios.get('http://localhost:8080/diets');
+    return data;
+};
+
+const fetchDifficulties = async () => {
+    const { data } = await axios.get('http://localhost:8080/difficulties');
+    return data;
+};
+
+export const useFetchCuisines = () => {
     return useQuery({
-        queryKey: ['recipes', page, limit],
-        queryFn: () => fetchRecipes(page, limit),
+        queryKey: ['cuisines'],
+        queryFn: fetchCuisines
+        
+    });
+};
+
+export const useFetchDiets = () => {
+    return useQuery({
+        queryKey: ['diets'],
+        queryFn: fetchDiets
+    });
+};
+
+export const useFetchDifficulties = () => {
+    return useQuery({
+        queryKey: ['difficulties'],
+        queryFn: fetchDifficulties
+    });
+};
+const fetchRecipes = async (page: number, limit: number, query: string, cuisine: string, difficulty: string, diet: string) => {
+    const params: any = { _page: page, _limit: limit };
+    if (query) params.q = query;
+    if (cuisine) params.cuisineId = cuisine;
+    if (difficulty) params.difficultyId = difficulty;
+    if (diet) params.dietId = diet;
+    const { data } = await axios.get(`http://localhost:8080/recipes`, { params });
+    return data;
+};
+
+const useRecipes = (page: number, limit: number, query: string, cuisine: string, difficulty: string, diet: string) => {
+    return useQuery({
+        queryKey: ['recipes', page, limit, query, cuisine, difficulty, diet],
+        queryFn: () => fetchRecipes(page, limit, query, cuisine, difficulty, diet),
     });
 };
 

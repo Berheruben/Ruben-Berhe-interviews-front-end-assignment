64fb615748a5f2bca36f9a902396cc2c53365c71
Berheruben <rubenberhe56@yourdomain.com>
2024-06-18 11:04:52 +0200

    file for importing components

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as RecipeList } from "./RecipeList";
+export { default as AddRecipe } from "./AddRecipe";
+export { default as RecipeDetails } from "./RecipeDetails";
+export { default as HomePage } from "./HomePage";

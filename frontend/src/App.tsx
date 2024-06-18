6146370092870c58b03fb8b232a2295eaa4011a0
Berheruben <rubenberhe56@yourdomain.com>
2024-06-18 11:05:57 +0200

    clean imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RecipeList from "./pages/RecipeList";
-import AddRecipe from "./pages/AddRecipe";
 import queryClient from "./api/query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import RecipeDetails from "./pages/RecipeDetails";
-import HomePage from "./components/HomePage";
+import { RecipeList, AddRecipe, RecipeDetails, HomePage } from "./pages";
 
 const App: React.FC = () => {
   return (
